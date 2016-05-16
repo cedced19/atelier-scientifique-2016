dc7b56e99abd28e1948d4bbef383e7c163292e30
Cédric <cedced19@gmail.com>
2016-05-16 17:47:53 +0200

    Do not format question and and create check answers system

--- vendor/javascripts/controllers/add-player.js
+++ vendor/javascripts/controllers/add-player.js
@@ -1,29 +1,41 @@
 module.exports = ['$scope', '$location', '$rootScope', 'localStorageService', 'notie', function($scope, $location, $rootScope, localStorageService, notie) {
         $rootScope.nav = 'add-player';
 
-        $scope.newPlayer = {};
-        $scope.questions = {
-          qcms: [],
-          numbers: [],
-          others: []
+        $scope.newPlayer = {
+          questions: []
         };
-
-        $rootScope.questions.forEach(function (item) {
-          if (item.type == 'qcm') {
-            $scope.questions.qcms.push(item);
-          } else if (item.type == 'number') {
-            $scope.questions.numbers.push(item);
-          } else {
-            $scope.questions.others.push(item);
-          }
-        });
 
 
         $scope.addPlayer = function () {
+          $rootScope.questions.forEach(function (item, key) {
+            if (!item.user || item.user == '') {
+              return $scope.newPlayer.questions.push({
+                answer: false
+              });
+            }
+
+            var correct = false;
+            if (item.type == 'qcm') {
+              if (item.user == item.verification) {
+                correct = true;
+              }
+            }
+            if (item.type == 'number') {
+              if (item.user <= item.verification.max && item.user >= item.verification.min) {
+                correct = true;
+              }
+            }
+            $scope.newPlayer.questions.push({
+              answer: item.user,
+              correct: correct
+            });
+            $rootScope.questions[key].user = '';
+          });
           var players = localStorageService.get('players');
           players.push({
             name: $scope.newPlayer.name,
-            firstName: $scope.newPlayer.firstName
+            firstName: $scope.newPlayer.firstName,
+            questions: $scope.newPlayer.questions
           });
           localStorageService.set('players', players);
           notie.alert(1, 'Un nouveau participant a été ajouté.', 2);
